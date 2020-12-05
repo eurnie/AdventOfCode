636f200b6f74ed7a40649312b271d5e7462c3e0d
Arne Vermeulen <vermeulen.arne@gmail.com>
2020-12-05 09:59:52 +0100

    puzzle2 of day5

---
+++ day5/puzzle2.py
@@ -0,0 +1,55 @@
+def getLowerHalf(tuple):
+	min = tuple[0]
+	max = tuple[1]
+	if ((max - min) == 1):
+		return (min, min)
+	else:
+		newMax = min + (round((max - min) / 2) - 1)
+		return (min, newMax)		
+
+def getUpperHalf(tuple):
+	min = tuple[0]
+	max = tuple[1]
+	if ((max - min) == 1):
+		return (max, max)
+	else:
+		newMin = min + (round((max - min) / 2))
+		return (newMin, max)
+
+def generateID(rowNB, columnNB):
+	return ((rowNB * 8) + columnNB)
+
+def checkMissingID(listOfID):
+	sortedList = sorted(listOfID)
+	min = sortedList[0]
+	max = sortedList[-1]
+	missing = 0
+	for x in range(min, max):
+		if (not (x in listOfID)):
+			missing = x
+			break
+	return missing
+
+with open('input.txt') as f:
+    lines = f.readlines()
+
+listOfID = []
+
+for line in lines:
+	row = (0, 127)
+	column = (0, 7)
+	for character in line:
+		if (character == "F"):
+			row = getLowerHalf(row)
+		elif (character == "B"):
+			row = getUpperHalf(row)
+		elif (character == "L"):
+			column = getLowerHalf(column)
+		elif (character == "R"):
+			column = getUpperHalf(column)
+
+	result = generateID(row[0], column[0])
+	listOfID.append(result)
+
+result = checkMissingID(listOfID)
+print(result)
