0d369db3bb8967ab963b8a6fe2b285a173f1dc26
Arne Vermeulen <vermeulen.arne@gmail.com>
2020-12-10 11:37:17 +0100

    add puzzle1 of day10 2020

---
+++ 2020/day10/puzzle1.py
@@ -0,0 +1,22 @@
+def createlistOfInput(input):
+	with open(input) as f:
+		lines = f.readlines()
+	newList = [0]
+	for line in lines:
+		splitted = int(line[:-1])
+		newList.append(splitted)
+	return newList
+
+def calculateResult(inputList):
+	inputList.sort()
+	numberOf1 = 0
+	numberOf3 = 0
+	for x in range(1, len(inputList)):
+		if ((inputList[x] - inputList[x-1]) == 1):
+			numberOf1 += 1
+		elif ((inputList[x] - inputList[x-1]) == 3):
+			numberOf3 += 1
+	numberOf3 += 1
+	return (numberOf1 * numberOf3)
+
+print(calculateResult(createlistOfInput('input.txt')))
