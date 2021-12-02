965491d1d730b1e2fde14826f8251c56cd305cb3
Arne Vermeulen <vermeulen.arne@gmail.com>
2021-12-02 15:14:13 +0100

    add puzzle2 of day2 2021

--- 2021/02/puzzle2.py
+++ 2021/02/puzzle2.py
@@ -1,3 +1,20 @@
 with open('input.txt') as f:
     lines = f.readlines()
 
+horizontal = 0
+depth = 0
+aim = 0
+
+for line in lines:
+    line_splitted = line.split(' ');
+    if (line_splitted[0] == 'forward'):
+        horizontal += int(line_splitted[1])
+        depth += aim * int(line_splitted[1])
+
+    if (line_splitted[0] == 'down'):
+        aim += int(line_splitted[1])
+
+    if (line_splitted[0] == 'up'):
+        aim -= int(line_splitted[1])
+
+print(horizontal * depth)
